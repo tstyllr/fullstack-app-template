8011e909611aacc768666a45c1a0a25e84d658b0
yangzhicong <770633567@qq.com>
2025-10-01 22:52:21 +0800

    Run backend and frontend concurrently

--- index.ts
+++ index.ts
@@ -1,1 +1,16 @@
-console.log('Hello via Bun!');
+import concurrently from 'concurrently';
+
+concurrently([
+   {
+      name: 'backend',
+      command: 'bun run dev',
+      cwd: 'backend',
+      prefixColor: 'cyan',
+   },
+   {
+      name: 'frontend',
+      command: 'bun run web',
+      cwd: 'frontend',
+      prefixColor: 'green',
+   },
+]);
