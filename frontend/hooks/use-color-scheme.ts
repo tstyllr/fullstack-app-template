d866c17fd05af598c84dd7bb500f30cf1ba598ec
yangzhicong <770633567@qq.com>
2025-10-12 18:08:23 +0800

    ThemedInput重构

--- frontend/hooks/use-color-scheme.ts
+++ frontend/hooks/use-color-scheme.ts
@@ -1,16 +1,20 @@
 import { useContext } from 'react';
-import { useColorScheme as useSystemColorScheme } from 'react-native';
+import {
+   ColorSchemeName,
+   useColorScheme as useSystemColorScheme,
+} from 'react-native';
 import { ThemeContext } from '@/components/molecules/theme-context';
 
 export function useColorScheme() {
    const themeContext = useContext(ThemeContext);
    const systemColorScheme = useSystemColorScheme();
+   const defaultColorScheme: ColorSchemeName = 'light';
 
    // 如果在 ThemeProvider 内部，使用其提供的 colorScheme
    if (themeContext) {
-      return themeContext.colorScheme;
+      return themeContext.colorScheme || defaultColorScheme;
    }
 
    // 否则回退到系统主题
-   return systemColorScheme;
+   return systemColorScheme || defaultColorScheme;
 }
